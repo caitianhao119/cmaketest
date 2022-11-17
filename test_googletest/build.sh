21625ce53a86f1dd4353c5a6fd31a3c35c97a9c8
bobby <191463980@qq.com>
2022-11-17 13:47:46 +0800

    cmake project with googletest

---
+++ test_googletest/build.sh
@@ -0,0 +1,9 @@
+if [ ! -d "./build/" ];then
+  mkdir build
+  else
+  rm -rf build
+  mkdir build
+fi
+cd build
+cmake ../
+make
---
+++ test_googletest/lib/CMakeFiles/sum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sum.dir/sum_integers.cpp.o"
+  "CMakeFiles/sum.dir/sum_integers.cpp.o.d"
+  "libsum.a"
+  "libsum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
